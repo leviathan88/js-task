6186b5535e0360f8db9d62f3046602293d8c9e81
Elvin Bahtiyar <elvinbextiyarov28@gmail.com>
2024-04-30 21:21:00 +0400

    name

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstName = prompt('What is your name?')
+var lastName = prompt('What is your surname?')
+var birthYear = prompt('Which year were you born?')
+// let date = new Date()
+// let year = date.getFullYear()
+// let age = year - birthYear
+var age = 2024 - birthYear
+alert(`${firstName} ${lastName} ${age} years old`)
+console.log(`${firstName} ${lastName} ${age} years old`)
